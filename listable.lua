7968c417933b2abe6cca40fead473fb0f112cc23
Gordon Brander <marlfoskr@gmail.com>
2014-10-12 15:57:07 -0700

    Add extend and merge functions

--- listable.lua
+++ listable.lua
@@ -70,4 +70,20 @@ local function zip_with(a, b, combine)
 end
 exports.zip_with = zip_with
 
+local function extend(a, b)
+  -- Set values of b on a, mutating a
+  -- Returns a
+  for k, v in pairs(b) do a[k] = v end
+  return a
+end
+exports.extend = extend
+
+local function merge(a, b)
+  -- Combine keys and values of a and b in new table.
+  -- b's keys will overwrite a's keys when a conflict arises.
+  -- Returns new table.
+  return extend(extend({}, a), b)
+end
+exports.merge = merge
+
 return exports
