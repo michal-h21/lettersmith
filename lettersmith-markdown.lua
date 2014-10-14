26d71d3dcdb104cc493596a9abd4e021317e419f
Gordon Brander <marlfoskr@gmail.com>
2014-10-13 21:59:08 -0700

    Add doc comments to markdown plugin

--- lettersmith-markdown.lua
+++ lettersmith-markdown.lua
@@ -1,3 +1,16 @@
+--[[
+Lettersmith Markdown
+
+Renders markdown files (.md, .markdown, .mdown).
+
+Usage:
+
+    local markdown = require('lettersmith-markdown')
+    local lettersmith = require('lettersmith')
+
+    local docs = lettersmith.docs('raw')
+    build(markdown(docs))
+--]]
 local markdown = require('discount')
 
 local lettersmith = require('lettersmith')
