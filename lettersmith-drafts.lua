1d89541d59b780c3f2c3380fc524d6c53a996ec9
Gordon Brander <marlfoskr@gmail.com>
2014-10-15 11:53:50 -0700

    Add doc comment to lettersmith-drafts

--- lettersmith-drafts.lua
+++ lettersmith-drafts.lua
@@ -1,3 +1,20 @@
+--[[
+Lettersmith Drafts
+
+Remove drafts from rendered docs. A drafts is any document that has `draft: yes`
+in the headmatter section.
+
+Usage:
+
+    local use_drafts = require('lettersmith-drafts')
+    local lettersmith = require('lettersmith')
+
+    local docs = lettersmith.docs('raw')
+
+    docs = use_drafts(docs)
+
+    build(docs, "out")
+--]]
 local list = require('colist')
 local reject = list.reject
 
