d109c2b27e9d5d6c5e4d297cf602b6f41a43300b
Gordon Brander <marlfoskr@gmail.com>
2014-10-25 14:34:59 -0700

    Export table rather than function

--- lettersmith-collections.lua
+++ lettersmith-collections.lua
@@ -20,6 +20,7 @@ local streams = require('colist')
 local map = streams.map
 local filter = streams.filter
 local collect = streams.collect
+local take = streams.take
 
 local date = require("date")
 
@@ -27,25 +28,45 @@ local util = require('util')
 local merge = util.merge
 local shallow_copy = util.shallow_copy
 
-local function compare_doc_by_date(a_doc, b_doc)
+local mod = {}
+
+function mod.compare_doc_by_date(a_doc, b_doc)
   -- Compare 2 docs by date, reverse chronological.
   return date(a_doc.date) < date(b_doc.date)
 end
 
-return function (doc_stream, name, pattern, compare)
-  -- Add metadata to all documents.
-  -- Returns new list of documents with metadata mixed in.
-  -- Fields from document take precidence.
+function mod.query_and_collect(doc_stream, pattern, compare, limit)
+  -- Query document stream, returning a list table filtered by `pattern`,
+  -- ordered by `compare` and capped by `limit`.
+  -- Returns sorted list table of shallow copied doc objects.
+
+  -- Create new filtered stream containing only files that match pattern.
   local sub_path_stream = filter(doc_stream, function (doc)
     return doc.relative_filepath:find(pattern) ~= nil
   end)
 
-  -- Create shallow copies of doc objects for collection
-  local sub_path_copies = map(sub_path_stream, shallow_copy)
+  local capped = take(sub_path_stream, limit)
 
-  local collection = collect(sub_path_stream)
-  -- Sort collection
-  table.sort(collection, compare or compare_doc_by_date)
+  -- Create shallow copies of doc objects for collection
+  local shallow_copies = map(capped, shallow_copy)
+
+  local collection = collect(shallow_copies)
+
+  -- Sort collection in-place
+  table.sort(collection, compare)
+
+  return collection
+end
+
+function mod.query(doc_stream, pattern, compare, limit)
+  -- Just a nice wrapper for query and collect that provides some sensible
+  -- argument defaults.
+  return mod.query_and_collect(
+    doc_stream, pattern, compare or compare_doc_by_date, limit or math.huge)
+end
+
+function mod.use(doc_stream, name, pattern, compare, limit)
+  local collection = query(doc_stream, pattern, compare, limit)
 
   return map(doc_stream, function (doc)
     return merge(doc, {
@@ -53,3 +74,5 @@ return function (doc_stream, name, pattern, compare)
     })
   end)
 end
+
+return mod
