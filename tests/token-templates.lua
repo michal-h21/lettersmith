c8b2675c4f6ed15a839d4f45841ca47b74d0a7e6
Gordon Brander <marlfoskr@gmail.com>
2014-10-26 14:56:25 -0700

    Do not support - in token templates

--- tests/token-templates.lua
+++ tests/token-templates.lua
@@ -14,9 +14,17 @@ suite("render()", function ()
 
   equal(rendered, "Hello, my name is Joe Schmoe", "Renders template tokens")
 
-  local rendered2 = tokens.render(template, {
+  local rendered_2 = tokens.render(template, {
     first = "Joe"
   })
 
-  equal(rendered2, "Hello, my name is Joe ", "Renders empty strings for tokens with no match")
+  equal(rendered_2, "Hello, my name is Joe ", "Renders empty strings for tokens with no match")
+
+  local template_3 = ":foo-:bar"
+  local rendered_3 = tokens.render(template_3, {
+    foo = "hello",
+    bar = "world"
+  })
+
+  equal(rendered_3, "hello-world", "Tokens do not contain -")
 end)
--- token-templates.lua
+++ token-templates.lua
@@ -3,10 +3,12 @@ local exports = {}
 -- A simple template renderer that takes a string with optional `:key` tokens
 -- and renders replacements using associated values in `context_table`.
 --
--- `context_table` contains token, replacement pairs. Keys do not need to
--- contain :.
+-- `context_table` contains token, replacement pairs.
+--
+-- Tokens start with a `:` and may be followed by any number of alphanumeric
+-- characters or `_`.
 local function render(url_template_string, context_table)
-  return url_template_string:gsub(":([%w-_]+)", function(key)
+  return url_template_string:gsub(":([%w_]+)", function(key)
     return context_table[key] or ""
   end)
 end
