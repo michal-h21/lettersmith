d4cfb3b8e29983fdab9b464b8d237db73bebda92
Gordon Brander <marlfoskr@gmail.com>
2014-10-28 19:35:20 -0700

    Fix collection sorting using skim (not take)

--- lettersmith-collections.lua
+++ lettersmith-collections.lua
@@ -16,57 +16,55 @@ Usage:
 
     build(docs, "out")
 --]]
+local lettersmith = require("lettersmith")
+local query = lettersmith.query
+
 local streams = require("streams")
 local map = streams.map
 local filter = streams.filter
+local skim = streams.skim
+local values = streams.values
 local collect = streams.collect
-local take = streams.take
 
 local date = require("date")
 
-local util = require('util')
+local util = require("util")
 local merge = util.merge
 local shallow_copy = util.shallow_copy
 
-local mod = {}
+local exports = {}
 
-function mod.compare_doc_by_date(a_doc, b_doc)
+local function compare_doc_by_date(a_doc, b_doc)
   -- Compare 2 docs by date, reverse chronological.
   return date(a_doc.date) < date(b_doc.date)
 end
+exports.compare_doc_by_date = compare_doc_by_date
 
-function mod.query_and_collect(doc_stream, pattern, compare, limit)
-  -- Query document stream, returning a list table filtered by `pattern`,
-  -- ordered by `compare` and capped by `limit`.
+local function list_in_order(doc_stream, path_query_string, compare, n)
+  -- Query document stream, returning a list table filtered by path query
+  -- string, ordered by `compare` and capped by `n`.
   -- Returns sorted list table of shallow copied doc objects.
 
   -- Create new filtered stream containing only files that match pattern.
-  local sub_path_stream = filter(doc_stream, function (doc)
-    return doc.relative_filepath:find(pattern) ~= nil
-  end)
+  local matches = query(doc_stream, path_query_string)
 
-  local capped = take(sub_path_stream, limit)
+  local top_n = skim(matches, compare, n)
 
   -- Create shallow copies of doc objects for collection
-  local shallow_copies = map(capped, shallow_copy)
+  -- @TODO not ideal that we have to convert back to stream here. Maybe
+  -- create a set of table functions (map_table, etc).
+  local shallow_copies = map(values(top_n), shallow_copy)
 
-  local collection = collect(shallow_copies)
-
-  -- Sort collection in-place
-  table.sort(collection, compare)
-
-  return collection
+  return collect(shallow_copies)
 end
+exports.list_in_order = list_in_order
 
-function mod.query(doc_stream, pattern, compare, limit)
-  -- Just a nice wrapper for query and collect that provides some sensible
-  -- argument defaults.
-  return mod.query_and_collect(
-    doc_stream, pattern, compare or compare_doc_by_date, limit or math.huge)
-end
+local function use(doc_stream, name, path_query_string, compare, n)
+  -- Default to comparing files by date.
+  compare = compare or compare_doc_by_date
 
-function mod.use(doc_stream, name, pattern, compare, limit)
-  local collection = query(doc_stream, pattern, compare, limit)
+  -- Create collection of shallow copies
+  local collection = list_in_order(doc_stream, path_query_string, compare, n)
 
   return map(doc_stream, function (doc)
     return merge(doc, {
@@ -74,5 +72,6 @@ function mod.use(doc_stream, name, pattern, compare, limit)
     })
   end)
 end
+exports.use = use
 
-return mod
+return exports
