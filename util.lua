1cd253a5ad29807ed3590d795da6bfdf55c231f1
Gordon Brander <marlfoskr@gmail.com>
2014-10-28 19:29:10 -0700

    Add fold and map

--- util.lua
+++ util.lua
@@ -1,3 +1,8 @@
+--[[
+Table utilities: the goodies you might wish were part of the standard
+table library.
+]]--
+
 local exports = {}
 
 local function extend(a, b)
@@ -20,5 +25,29 @@ local function shallow_copy(t)
   return extend({}, t)
 end
 exports.shallow_copy = shallow_copy
+
+local function set(t, k, v)
+  t[k] = v
+  return t
+end
+exports.set = set
+
+local function fold(t, step, seed)
+  -- Fold a value from a `seed` using `step` function.
+  -- Call `step` with value and index, updating `seed` with return value.
+  for i, v in ipairs(t) do seed = step(seed, v, i) end
+  return seed
+end
+exports.fold = fold
+
+local function map(t, transform)
+  -- Map all values of table using function `transform`.
+  -- Returns new indexed table.
+  return fold(t, function (out, v, i)
+    -- Set transformed value on `out` table at `i` index.
+    return set(out, transform(v), i)
+  end, {})
+end
+exports.map = map
 
 return exports
