9ace36a68896c561fcadf86b9781138f32fc778c
Gordon Brander <marlfoskr@gmail.com>
2014-10-31 21:54:30 -0700

    Create circular links between collected items

--- lettersmith_collections.lua
+++ lettersmith_collections.lua
@@ -17,10 +17,12 @@ Usage:
     build(docs, "out")
 --]]
 local lettersmith = require("lettersmith")
-local query_docs = lettersmith.query
+local query = lettersmith.query
 
 local foldable = require("foldable")
+local fold = foldable.fold
 local map = foldable.map
+local folds = foldable.folds
 local harvest = foldable.harvest
 local collect = foldable.collect
 
@@ -38,29 +40,66 @@ local function compare_doc_by_date(a_doc, b_doc)
 end
 exports.compare_doc_by_date = compare_doc_by_date
 
-local function list_in_order(docs_foldable, path_query_string, compare, n)
-  -- Query document stream, returning a list table filtered by path query
-  -- string, ordered by `compare` and capped by `n`.
-  -- Returns sorted list table of shallow copied doc objects.
+-- Delay each value in a foldable value by one turn.
+-- Why? It's useful for producing `folds` where both arguments to the folding
+-- function are mutated. Sounds confusing, but it lets you do the mutation
+-- before anyone else can see it.
+-- As Clojure puts it "if a tree mutates in a forest and no one hears it...".
+local function detain(foldable)
+  return function(step, seed)
+    local v = fold(foldable, function (last, v)
+      if (last) then seed = step(seed, last) end
+      return v
+    end, nil)
 
-  -- Create new filtered stream containing only files that match pattern.
-  local matches = query_docs(docs_foldable, path_query_string)
+    return step(seed, v)
+  end
+end
 
-  local top_n = harvest(matches, compare, n)
-  -- Create shallow copies of doc objects for collection.
-  local top_n_shallow_copies = map(top_n, shallow_copy)
+local function set_circular_link(prev_t, next_t)
+  -- We're mutating both tables, so be wise and pass in tables that are not
+  -- owned by anyone else.
+  if prev_t then
+    prev_t.next = next_t
+    next_t.prev = prev_t
+    -- Set table index
+    next_t.number = prev_t.number + 1
+  else
+    next_t.number = 1
+  end
+  return next_t
+end
+
+-- Turn foldable full of tables into a circular linked list. Note that tables
+-- will be shallow copied before this is done, so the originals won't be
+-- mutated. We also detain each value, so the circular link mutation is never
+-- seen by anyone. Basically this means you can still fold tables "just in time"
+-- and they will have both the `prev` and `next` property when you receive them.
+local function link_circularly(tables_foldable)
+  return detain(folds(map(tables_foldable, shallow_copy), set_circular_link))
+end
+exports.link_circularly = link_circularly
+
+local function list_collection(docs_foldable, compare, n)
+  -- Harvest the top `n` sorted tables.
+  local top_n = harvest(docs_foldable, compare, n)
+
+  -- Create circular next/prev references between shallow copies.
+  local linked = link_circularly(top_n)
 
   -- Collect into indexed table
-  return collect(top_n_shallow_copies)
+  return collect(linked)
 end
-exports.list_in_order = list_in_order
+exports.list_collection = list_collection
 
 local function use(docs_foldable, name, path_query_string, compare, n)
   -- Default to comparing files by date.
   compare = compare or compare_doc_by_date
 
-  -- Create collection of shallow copies
-  local collection = list_in_order(docs_foldable, path_query_string, compare, n)
+  local matches =
+    query(docs_foldable, path_query_string, compare, n)
+
+  local collection = list_collection(matches, compare, n)
 
   return map(docs_foldable, function (doc)
     return merge(doc, {
