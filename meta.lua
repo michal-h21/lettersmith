d14094c306fd88df8012b17a100dc0d338249d57
Gordon Brander <marlfoskr@gmail.com>
2014-11-30 16:11:34 -0800

    Update meta to use reducers

--- meta.lua
+++ meta.lua
@@ -12,24 +12,17 @@ Usage:
 --]]
 local exports = {}
 
-local lazily = require("lettersmith.lazily")
-local transducers = require("lettersmith.transducers")
+local reducers = require("lettersmith.reducers")
+local map = reducers.map
 
 local table_utils = require("lettersmith.table_utils")
 local merge = table_utils.merge
 
-local function map_meta(meta)
-  return transducers.map(function (doc)
-    return merge(meta, doc)
-  end)
-end
-exports.map_meta = map_meta
-
 local function use_meta(meta)
-  local xf = map_meta(meta)
-
   return function (docs)
-    return lazily.transform(xf, docs)
+    return map(function (doc)
+      return merge(meta, doc)
+    end, docs)
   end
 end
 exports.use_meta = use_meta
