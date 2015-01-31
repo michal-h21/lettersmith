253e16a1b86164feabd70ef1df1a2ba723d7ba5b
Gordon Brander <marlfoskr@gmail.com>
2015-01-31 10:55:19 -0800

    Update meta plugin as simple xform

--- meta.lua
+++ meta.lua
@@ -2,29 +2,17 @@
 Lettersmith Meta
 
 Add metadata to every doc object. This is useful for things like site meta.
-
-Usage:
-
-    local use_meta = require('lettersmith.meta').use_meta
-    local lettersmith = require('lettersmith')
-
-    lettersmith.generate("raw", "out", use_meta { site_title = "My website" })
 --]]
-local exports = {}
-
-local reducers = require("lettersmith.reducers")
-local map = reducers.map
+local transducers = require('lettersmith.transducers')
+local map = transducers.map
 
 local table_utils = require("lettersmith.table_utils")
 local merge = table_utils.merge
 
 local function use_meta(meta)
-  return function (docs)
-    return map(function (doc)
-      return merge(meta, doc)
-    end, docs)
-  end
+  return map(function (doc)
+    return merge(meta, doc)
+  end)
 end
-exports.use_meta = use_meta
 
-return exports
+return use_meta
